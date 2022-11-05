33eab514965d6a16ab78d1d985fcf7451247bc30
Plasma <hwengerstickel@protonmail.com>
2022-11-05 18:50:56 -0500

    Show that we are scanning for packages

--- scripts/rm-old.sh
+++ scripts/rm-old.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # Thanks https://askubuntu.com/a/250166
+echo -e ":: SCANNING FOR OLD PACKAGES"
 dpkg-scanpackages --arch amd64 "/home/pacstall/ppr-base/pool/" 2>&1 >/dev/null | grep -Po '((\/.*?deb)(?=.*?repeat;))|used that.*?\K(\/.*deb)' | xargs rm
