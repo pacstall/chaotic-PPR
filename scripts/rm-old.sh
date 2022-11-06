6ba18a8759cddeaad4c03cbacf1c9a6ed68b47c3
Plasma <hwengerstickel@protonmail.com>
2022-11-06 08:22:32 -0600

    Fix xargs and wait.sh

--- scripts/rm-old.sh
+++ scripts/rm-old.sh
@@ -2,4 +2,5 @@
 
 # Thanks https://askubuntu.com/a/250166
 echo -e ":: SCANNING FOR OLD PACKAGES"
-dpkg-scanpackages --arch amd64 "/home/pacstall/ppr-base/pool/" 2>&1 >/dev/null | grep -Po '((\/.*?deb)(?=.*?repeat;))|used that.*?\K(\/.*deb)' | xargs rm
+# xargs -r prevents running if empty
+dpkg-scanpackages --arch amd64 "/home/pacstall/ppr-base/pool/" 2>&1 >/dev/null | grep -Po '((\/.*?deb)(?=.*?repeat;))|used that.*?\K(\/.*deb)' | xargs -r rm
