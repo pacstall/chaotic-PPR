51081e6c87c6e02b5d1c2a94b3f868288ee8d2c8
Plasma <hwengerstickel@protonmail.com>
2022-11-06 14:21:55 -0600

    fix(wait.sh): cd into proper dir

--- scripts/wait.sh
+++ scripts/wait.sh
@@ -9,6 +9,7 @@ while inotifywait -e modify -e move -e create -e delete /home/pacstall/ppr-base/
 	"$SCRIPT_DIR/rm-old.sh"
 	# Continue to add packages
 	echo -e ":: PACKAGE DIR HAS BEEN MODIFIED"
+	cd /home/pacstall/ppr-base/
 	dpkg-scanpackages --arch amd64 pool/ > "/home/pacstall/ppr-base/dists/pacstall/main/binary-amd64/Packages"
 	cat "/home/pacstall/ppr-base/dists/pacstall/main/binary-amd64/Packages" | gzip -9 > "/home/pacstall/ppr-base/dists/pacstall/main/binary-amd64/Packages.gz"
 	cd "/home/pacstall/ppr-base/dists/pacstall"
