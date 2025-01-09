63c3f19bb388b9e63f5de5145e44980f83e78d62
ook37 <oren+12345@taumoda.com>
2025-01-09 03:25:05 -0500

    ref: make port setup config

--- scripts/creator.sh
+++ scripts/creator.sh
@@ -21,7 +21,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Pacstall. If not, see <https://www.gnu.org/licenses/>.
 
-LOCAL_PORT=8088
+APTLY_PORT="${1:?need PORT}"
 distrolist=("main" "ubuntu-latest" "ubuntu-develop" "ubuntu-rolling" "debian-stable" "debian-testing" "debian-unstable")
 archlist=("amd64" "arm64" "source")
 mapfile -t pprlist < <(for i in "${distrolist[@]}"; do echo "{\"Component\": \"${i}\", \"Name\": \"ppr-${i}\"},"; done)
@@ -32,10 +32,10 @@ archstring="$(printf '"%s", ' "${archlist[@]}")"
 for i in "${distrolist[@]}"; do
   curl -X POST -H 'Content-Type: application/json' \
     --data "{\"Name\": \"ppr-${i}\", \"DefaultDistribution\": \"pacstall\", \"DefaultComponent\": \"${i}\"}" \
-    http://localhost:"${LOCAL_PORT}"/api/repos
+    http://localhost:"${APTLY_PORT}"/api/repos
 done
 
 # publish
 curl -X POST -H 'Content-Type: application/json' \
   --data "{\"SourceKind\": \"local\", \"Sources\": [${pprstring%,}], \"Architectures\": [${archstring%, }], \"Distribution\": \"pacstall\", \"Signing\": {\"Skip\": true}, \"MultiDist\": true}" \
-  http://localhost:"${LOCAL_PORT}"/api/publish/pacstall
+  http://localhost:"${APTLY_PORT}"/api/publish/pacstall
